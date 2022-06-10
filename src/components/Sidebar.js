972e829c3016af308d86c0863d0b86302eaf8702
Dylan <dylan.su1109@gmail.com>
2022-06-10 23:12:55 +0800

    sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,30 @@
+function Sidebar() {
+  return (
+    <>
+      <ul class="nav flex-column">
+        <li class="nav-item">
+          <a class="nav-link" href="/#">
+            課程介紹
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/#">
+            課程說明
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/#">
+            購買須知
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#/">
+            商品購買
+          </a>
+        </li>
+      </ul>
+    </>
+  )
+}
+
+export default Sidebar
--- src/pages/Course.js
+++ src/pages/Course.js
@@ -1,29 +1,9 @@
-import './_course.scss'
+import '../styles/_course.scss'
+import Sidebar from '../components/Sidebar'
 function Course() {
   return (
     <>
-      <ul class="nav flex-column">
-        <li class="nav-item">
-          <a class="nav-link" href="/#">
-            課程介紹
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/#">
-            課程說明
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/#">
-            購買須知
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#/">
-            商品購買
-          </a>
-        </li>
-      </ul>
+      <Sidebar />
     </>
   )
 }
