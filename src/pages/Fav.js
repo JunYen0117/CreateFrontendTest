63ad607b0d96c29abca2d674265fc1c1b2ebb145
Miu37 <ispan8937@gmail.com>
2022-06-10 18:29:03 +0800

    update

--- src/pages/Fav.js
+++ src/pages/Fav.js
@@ -1,10 +1,17 @@
-import React from 'react'
 import '../styles/_fav.scss'
 
 const Fav = () => {
   return (
     <>
-      <div className="">Fav</div>
+      <h1>我的訂單</h1>
+      <div className='border-bottom'>
+        <a href="#" className=' active'>已完成</a>
+        <a href="#" className=''>已取消</a>
+      </div>
+
+      <div>
+
+      </div>
     </>
   )
 }
