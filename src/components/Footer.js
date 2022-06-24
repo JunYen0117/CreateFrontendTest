1e6ff1a2d651ad83af84f51b218e05aeeaa1231a
JunYen0117 <relife12345@gmail.com>
2022-06-24 20:05:22 +0800

    add files from 正式專案的dev

---
+++ src/components/Footer.js
@@ -0,0 +1,174 @@
+import { Accordion } from 'react-bootstrap';
+import { ReactComponent as IGLogo } from '../img/footer/IGLogo.svg';
+import { ReactComponent as FBLogo } from '../img/footer/FBLogo.svg';
+import { ReactComponent as LineLogo } from '../img/footer/LineLogo.svg';
+import Logo2 from '../img/footer/Logo2.svg';
+
+function Footer() {
+  return (
+    <>
+      {/* 以下為桌機版畫面 */}
+      <footer className="footer_desktop container-fluid d-none d-md-block text-white h4 row">
+        <div className="container footer_all ms-5 col-11">
+          {/* footer_up */}
+          <ul className="footer_up_title d-flex pt-4 footer_top ms-5">
+            <li>活動資訊</li>
+            <li>幫助</li>
+            <li>認識</li>
+            <li className="small_item">社群</li>
+          </ul>
+          <ul className="footer_up_text d-flex py-4 footer_top ms-5">
+            <li>
+              <p>電話：03-400000</p>
+              <p>地址：桃園市中壢區新生路二段421號</p>
+              <p>信箱：color123@example.com</p>
+            </li>
+            <li>
+              <p>
+                <a href="#/" className="footer_a">
+                  Q&A
+                </a>
+              </p>
+              <p>
+                <a href="#/" className="footer_a">
+                  客服
+                </a>
+              </p>
+            </li>
+            <li>
+              <p>
+                <a href="#/" className="footer_a">
+                  關於迴廊
+                </a>
+              </p>
+              <p>
+                <a href="#/" className="footer_a">
+                  Business
+                </a>
+              </p>
+            </li>
+            <li className="small_item d-flex">
+              <div className="mx-2">
+                <a href="#/" className="footer_a">
+                  <IGLogo />
+                </a>
+              </div>
+              <div className="mx-2">
+                <a href="#/" className="footer_a">
+                  <FBLogo />
+                </a>
+              </div>
+              <div className="mx-2">
+                <a href="#/" className="footer_a">
+                  <LineLogo />
+                </a>
+              </div>
+            </li>
+          </ul>
+
+          {/* footer_down */}
+          <div className="footer_down d-flex justify-content-between mt-3 ms-5">
+            <ul className="footer_down_left d-flex footer_top">
+              <li className="me-2">
+                <a href="#/" className="footer_a">
+                  <img src={Logo2} alt="" />
+                </a>
+              </li>
+              <li className="create_your_life align-self-center">
+                Create your life
+              </li>
+            </ul>
+            <ul className="footer_down_right d-flex pt-3 footer_top">
+              <li className="ms-3">©2022</li>
+              <li className="ms-3">
+                <a href="#/" className="footer_a">
+                  隱私權政策
+                </a>
+              </li>
+              <li className="ms-3">
+                <a href="#/" className="footer_a">
+                  服務條款
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+      {/* 桌機版畫面結束 */}
+
+      {/* 以下為手機版畫面 */}
+      <footer className="footer_mobile d-flex flex-column d-md-none">
+        {/* 以下為bootstrap accordion套件 */}
+        <Accordion>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>聯絡資訊</Accordion.Header>
+            <Accordion.Body>
+              <p>電話：03-400000</p>
+              <p>地址：桃園市中壢區新生路二段421號</p>
+              <p>信箱：color123@example.com</p>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>幫助</Accordion.Header>
+            <Accordion.Body>
+              <p>
+                <a href="#/">Q&A</a>
+              </p>
+              <p>
+                <a href="#/">客服</a>
+              </p>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header>認識</Accordion.Header>
+            <Accordion.Body>
+              <p>
+                <a href="#/">關於迴廊</a>
+              </p>
+              <p>
+                <a href="#/">Business</a>
+              </p>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+        {/* bootstrap accordion套件結束 */}
+
+        <div className="d-flex justify-content-center mt-3">
+          <div className="mx-3">
+            <a href="#/" className="footer_a">
+              <IGLogo />
+            </a>
+          </div>
+          <div className="mx-3">
+            <a href="#/" className="footer_a">
+              <FBLogo />
+            </a>
+          </div>
+          <div className="mx-3">
+            <a href="#/" className="footer_a">
+              <LineLogo />
+            </a>
+          </div>
+        </div>
+        <div className="d-flex py-2">
+          <p className="create_your_life mx-auto mt-2">Create your life.</p>
+        </div>
+        <div className="d-flex justify-content-center pb-2">
+          <div className="mx-4">©2022</div>
+          <div className="mx-4">
+            <a href="#/" className="footer_a">
+              隱私權政策
+            </a>
+          </div>
+          <div className="mx-4">
+            <a href="#/" className="footer_a">
+              服務條款
+            </a>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,208 @@
+import { ReactComponent as LogoSvg } from '../img/header/logo.svg';
+import LogoDesktop from '../img/header/logo.svg';
+import { ReactComponent as Search } from '../img/header/search.svg';
+import { ReactComponent as Shopcart1 } from '../img/header/shopcart1.svg';
+import { ReactComponent as NoLogin } from '../img/header/nologin.svg';
+import { ReactComponent as Hamburger } from '../img/header/Hamburger.svg';
+import { ReactComponent as MobileSearch } from '../img/header/MobileSearch.svg';
+import { ReactComponent as More } from '../img/header/More.svg';
+
+import { Link, NavLink, useHistory } from 'react-router-dom';
+import styled from '@emotion/styled';
+
+const LogoMobile = styled(LogoSvg)`
+  width: 90px;
+`;
+
+function Header() {
+  const history = useHistory();
+
+  return (
+    <>
+      <header className="container-fluid">
+        <div className="d-none d-md-flex justify-content-between">
+          <div className="d-flex ms-5">
+            <figure className="desktop_figure mx-5 mt-4">
+              <span
+                className="header_a"
+                onClick={() => {
+                  history.push('/');
+                }}
+              >
+                <img src={LogoDesktop} alt="" />
+              </span>
+            </figure>
+            <li className="search_item d-none d-md-block mt-5">
+              <input className="search_bar" type="text" />
+              <a href="#/" className="search_icon">
+                <Search />
+              </a>
+            </li>
+          </div>
+          <ul className="header_top menu1 d-none d-md-flex pt-4 ">
+            <li className="shopcart_item mt-3 me-3">
+              <Link to="/cart">
+                <Shopcart1 />
+              </Link>
+            </li>
+            <li className="profile_item mt-3 me-5">
+              <NoLogin />
+            </li>
+          </ul>
+        </div>
+        <ul className="menu2 d-none d-md-flex justify-content-around pt-4">
+          <li>
+            <Link to="#/">主題企劃</Link>
+          </li>
+          <li>
+            <Link to="#/">活動資訊</Link>
+          </li>
+          <li>
+            <NavLink to="/product" exact>
+              購物商城
+            </NavLink>
+          </li>
+          <li>
+            <Link to="#/">文章專欄</Link>
+          </li>
+          <li>
+            <Link to="#/">關於我們</Link>
+          </li>
+        </ul>
+
+        {/* 以下為手機版 */}
+        <figure className="mobile_figure d-md-none mx-auto">
+          <a href="#/" className="header_a">
+            <LogoMobile />
+          </a>
+        </figure>
+        <ul className="mobile_menu d-flex justify-content-between d-md-none">
+          <figure>
+            <a
+              data-bs-toggle="offcanvas"
+              href="#offcanvas_left"
+              role="button"
+              aria-controls="offcanvasExample"
+              className="header_a"
+            >
+              <Hamburger />
+            </a>
+          </figure>
+          <ul className="d-flex">
+            <figure className="me-3">
+              <a href="#/" className="header_a">
+                <MobileSearch />
+              </a>
+            </figure>
+            <figure>
+              <a
+                data-bs-toggle="offcanvas"
+                href="#offcanvas_right"
+                role="button"
+                aria-controls="offcanvasExample"
+                className="header_a"
+              >
+                <More />
+              </a>
+            </figure>
+          </ul>
+        </ul>
+
+        {/* offcanvas效果 */}
+        <div
+          className="offcanvas_left offcanvas offcanvas-start d-md-none"
+          tabIndex="-1"
+          id="offcanvas_left"
+          aria-labelledby="offcanvasExampleLabel"
+        >
+          <ul className="offcanvas_left_menu offcanvas-body d-flex flex-column justify-content-around">
+            {/* li包的button是關閉canvas的按鈕 */}
+            <li className="d-none">
+              <button
+                type="button"
+                className="btn-close text-reset"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
+            </li>
+            <li>
+              <a href="#/" className="header_a">
+                主題企劃
+              </a>
+            </li>
+            <li>
+              <a href="#/" className="header_a">
+                活動資訊
+              </a>
+            </li>
+            <li>
+              <a href="#/" className="header_a">
+                購物商城
+              </a>
+            </li>
+            <li>
+              <a href="#/" className="header_a">
+                文章專欄
+              </a>
+            </li>
+            <li className="last_li">
+              <a href="#/" className="header_a">
+                關於我們
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div
+          className="offcanvas_left offcanvas offcanvas-end d-md-none"
+          tabIndex="-1"
+          id="offcanvas_right"
+          aria-labelledby="offcanvasExampleLabel"
+        >
+          <ul className="offcanvas_left_menu offcanvas-body d-flex flex-column justify-content-around">
+            {/* li包的button是關閉canvas的按鈕 */}
+            <li className="d-none">
+              <button
+                type="button"
+                className="btn-close text-reset"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
+            </li>
+            <li>
+              <a href="#/" className="header_a">
+                購物車
+              </a>
+            </li>
+            <li>
+              <a href="#/" className="header_a">
+                我的活動
+              </a>
+            </li>
+            <li>
+              <a href="#/" className="header_a">
+                我的收藏
+              </a>
+            </li>
+            <li>
+              <a href="#/" className="header_a">
+                帳戶設定
+              </a>
+            </li>
+            <li>
+              <a href="#/" className="header_a">
+                變更密碼
+              </a>
+            </li>
+            <li className="last_li">
+              <a href="#/" className="header_a">
+                我的訂單
+              </a>
+            </li>
+          </ul>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
